76980684030fb7a7d99d1d009054656f514bbe99
DarrelA <darrelaiscoding@gmail.com>
2022-03-31 11:40:02 +0800

    Server structure refactor without using express.Router()

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,9 @@
 const express = require('express');
-const { access } = require('fs');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-require('dotenv').config();
+require('./services/passport');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
-    },
-
-    function (accessToken, refreshToken, profile, done) {
-      console.log({ accessToken });
-      console.log({ refreshToken });
-      console.log({ profile });
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile'] })
-);
-
-app.get(
-  '/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  }
-);
+require('./routes/authRoutes')(app);
 
 app.listen(port, () => console.log(`Emaily app listening on port ${port}`));
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile'] })
+  );
+
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/');
+    }
+  );
+};
